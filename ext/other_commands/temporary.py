a3b7c2431928bfe2c61326f54887e4cb31465a22
Irene Adler❤ <aluerie@outlook.com>
2025-08-07 20:41:35 +0300

    🔃Update `!runs`

--- ext/other_commands/temporary.py
+++ ext/other_commands/temporary.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Literal, TypedDict
 
 from twitchio.ext import commands
 
@@ -10,8 +10,41 @@ from utils import const
 if TYPE_CHECKING:
     from bot import IreBot, IreContext
 
+    class RunDict(TypedDict):
+        keywords: list[str]
+        game: str
+        desc: str
+
 
 __all__ = ("TemporaryCommands",)
+
+RUNS: list[RunDict] = [
+    {
+        "keywords": ["sekiro", "sekiro1"],
+        "game": "Sekiro",
+        "desc": (
+            "Bosses I like% (BIL%) "
+            "No Hit (NH) "
+            "Sword+Shuriken Focused (SSF) "
+            "No Kuro Charm (NKC) "
+            "Bell Demon (BD) "
+            "Hybrid Speed (HS) "
+            "Loopless (LL) "
+            "No Cheese (NC) "
+            "Glitchless (GL)"
+        ),
+    },
+    {
+        "keywords": ["sekiro", "sekiro2"],
+        "game": "Sekiro",
+        "desc": "Shura No Hit (NH) Speedrun (SR) Glitchless (GL)",
+    },
+    {
+        "keywords": ["er", "elden", "ring"],
+        "game": "Elden Ring",
+        "desc": "Twin Prodigies% (+Any% ?) Hybrid Speed/Hitless with my own !rules",
+    },
+]
 
 
 class TemporaryCommands(IreComponent):
@@ -38,41 +71,35 @@ class TemporaryCommands(IreComponent):
             raise payload.exception
 
     @commands.command()
-    async def run(self, ctx: IreContext, *, game: Literal["sekiro", "er"]) -> None:
-        """Get a description of a run I'm doing rn in a specified game."""
-        mapping = {
-            "sekiro": (
-                "Bosses I like% (BIL%) "
-                "No Hit (NH) "
-                "Sword+Shuriken Focused (SSF) "
-                "No Kuro Charm (NKC) "
-                "Bell Demon (BD) "
-                "Hybrid Speed (HS) "
-                "Loopless (LL) "
-                "No Cheese (NC) "
-                "Glitchless (GL)"
-            ),
-            "er": "Twin Prodigies% (+Any% ?) Hybrid Speed/Hitless with my own !rules",
-        }
-        await ctx.send(mapping[game.lower()])
+    async def runs(self, ctx: IreContext) -> None:
+        """Get a description of my all planned runs."""
+        for counter, run in enumerate(RUNS, start=1):
+            text = f"#{counter} {run['game']} : {run['desc']}"
+            await ctx.send(text)
 
-    @notes.error
-    async def run_error(self, payload: commands.CommandErrorPayload) -> None:
-        """Run Error."""
-        if isinstance(exc := payload.exception, commands.BadArgument):
-            await payload.context.send(f'Bad argument: "{exc.value}". Supported notes: "sekiro", "er".')
-        else:
-            raise payload.exception
+    @commands.command()
+    async def run(self, ctx: IreContext, *, query: str) -> None:
+        """Get a description of a run I'm doing rn in a specified game."""
+        query = query.lower()
+
+        run_found = False
+        for run in RUNS:
+            if query in run["keywords"]:
+                await ctx.send(run["desc"])
+                run_found = True
+
+        if not run_found:
+            await ctx.send(f'Your query "{query}" returned 0 results. You can just use !runs to see all planned runs.')
 
     @commands.command()
     async def rules(self, ctx: IreContext) -> None:
         """ER Run rules."""
         msg = (
             'Hybrid challenge "the best of both Speed/Hitless"'
-            " 1️⃣Aiming at least possible time in running sections: "
-            f"only speedrun leaderboard tier picks-ups are allowed {const.STV.Speedge}"
+            f" 1️⃣Minimize time in running sections: only speed-leaderboard picks-ups are allowed {const.STV.Speedge}"
             f" 2️⃣Only hits vs bosses count {const.STV.actually}"
             f" 3️⃣We use a custom-made starting class with fashion/weapon I choose {const.STV.forsenCD}"
+            f" 4️⃣No direct damage buffs allowed {const.STV.POLICE}"
         )
         await ctx.send(msg)
 
@@ -161,11 +188,7 @@ class TemporaryCommands(IreComponent):
     @commands.command()
     async def vilehand(self, ctx: IreContext) -> None:
         """Vilehand."""
-        msg = (
-            "omg how to deflect Vilehand's Sabimaru omg "
-            "fml chat omg "
-            "HOOOOOOOOOOOW omg "
-        )
+        msg = "omg how to deflect Vilehand's Sabimaru omg fml chat omg HOOOOOOOOOOOW omg "
         await ctx.send(msg)
 
     @commands.command()
