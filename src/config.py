c072c2163a0c2ca8ae32a4feb94c5ee74658e6ed
Aluerie <aluerie@gmail.com>
2026-04-22 04:31:55 +0300

    Test pre-commit

--- src/config.py
+++ src/config.py
@@ -8,23 +8,14 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from types_.config import Config
 
-__all__ = (
-    "config",
-    "replace_secrets",
-)
+__all__ = ("config", "replace_secrets")
 
 
 with Path("config.toml").open("rb") as fp:
     config: Config = tomllib.load(fp)  # pyright: ignore[reportAssignmentType]
 
 
-DO_NOT_SPOIL_LIST: list[str] = [
-    config["TWITCH"]["CLIENT_ID"],
-    config["TWITCH"]["CLIENT_SECRET"],
-    config["TOKENS"]["STEAM"],
-]
-
-
+DO_NOT_SPOIL_LIST: list[str] = [config["TWITCH"]["CLIENT_ID"], config["TWITCH"]["CLIENT_SECRET"], config["TOKENS"]["STEAM"]]
 DO_NOT_SPOIL_PATTERN = re.compile("|".join(map(re.escape, DO_NOT_SPOIL_LIST)))
 
 
