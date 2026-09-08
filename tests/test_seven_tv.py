ff358f7642c005e615ec496d377d45d81f2eeab6
Aluerie <aluerie@gmail.com>
2026-09-08 14:48:27 +0300

    Fix deploy

--- tests/test_seven_tv.py
+++ tests/test_seven_tv.py
@@ -1,16 +1,18 @@
 import pytest
 
-from utils import const, seven_tv_api
+from config import env
+from shared import seven_tv
+from utils import const
 
 pytest_plugins = ("pytest_asyncio",)
 
 
 @pytest.fixture
-async def stv() -> seven_tv_api.SevenTVClient:
-    return seven_tv_api.SevenTVClient()
+async def stv() -> seven_tv.SevenTVClient:
+    return seven_tv.SevenTVClient(env.SEVEN_TV_BEARER)
 
 
 @pytest.mark.asyncio
-async def test_active_emote_set_by_broadcaster(stv: seven_tv_api.SevenTVClient) -> None:
+async def test_active_emote_set_by_broadcaster(stv: seven_tv.SevenTVClient) -> None:
     irene_stv_id: str = await stv.user_get_active_emote(broadcaster_id=const.UserID.Irene)
     assert irene_stv_id == const.STV_IRENE_DEFAULT_EMOTE_SET_ID
