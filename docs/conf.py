e5a6bb5d67f83594a99c1fb07584f00286dfacaa
Aluerie <aluerie@gmail.com>
2026-09-17 15:41:28 +0300

    Idk

--- docs/conf.py
+++ docs/conf.py
@@ -5,36 +5,60 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path('..', 'src').resolve()))
+# I don't understand how to solve Pydantic + Sphinx mess properly
+# os.environ["PROJECT_NAME"] = ""
+# os.environ["TWITCH_CLIENT_ID"] = ""
+# os.environ["TWITCH_CLIENT_SECRET"] = ""
+# os.environ["POSTGRES_VPS"] = ""
+# os.environ["POSTGRES_HOME"] = ""
+# os.environ["STEAM_FRIEND_IRENE_ID64"] = "1"
+# os.environ["STEAM_FRIEND_IRENE_ID32"] = "1"
+# os.environ["STEAM_IRENESTEST_USERNAME"] = ""
+# os.environ["STEAM_IRENESTEST_PASSWORD"] = ""
+# os.environ["STEAM_IRENESBOT_USERNAME"] = ""
+# os.environ["STEAM_IRENESBOT_PASSWORD"] = ""
+# os.environ["STRATZ_BEARER"] = ""
+# os.environ["STEAM_API_KEY"] = ""
+# os.environ["SEVEN_TV_BEARER"] = ""
+# os.environ["SPOTIFY_AIDENWALLIS"] = ""
+# os.environ["EVENTSUB"] = ""
+# os.environ["WEBHOOK_LOGGER"] = ""
+# os.environ["WEBHOOK_ERROR"] = ""
+# os.environ["WEBHOOK_STREAM_NOTIFS"] = ""
+# os.environ["WEBHOOK_HEARTBEAT"] = ""
+# os.environ["WEBHOOK_HEARTBEAT"] = ""
 
-project = 'IreBot'
-copyright = '2020, Aluerie'
-author = 'Aluerie'
+sys.path.insert(0, str(Path("..", "src").resolve()))
 
-release = '0.1'
-version = '0.1.0'
+project = "IreBot"
+copyright = "2020, Aluerie"
+author = "Aluerie"
+
+release = "0.1"
+version = "0.1.0"
+
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
